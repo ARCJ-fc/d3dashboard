e03ca0a331b51585f03088fff70c1629856c8b18
Rory Sedgwick <rorysedgwick@Rorys-MacBook-Pro.local>
2015-02-24 17:08:36 +0000

    updates to server.js

--- server.js
+++ server.js
@@ -3,35 +3,40 @@
 var http = require('http'),
 	fs   = require('fs'),
 	path = require('path')
-	twitter = require('Twitter')
-	twitObj = new Twitter;
+	Twitter = require('twitter')
+	client = new Twitter ({
+	consumer_key: '0NE1Xoy4LOwiRuPBQYOZxAdsy',
+	consumer_secret: 'FwlWrejjFsJzh57JSd1OXlVFi2djcghERQNhHWsYZkgCLKUVxZ',
+	access_token_key: '281746499-SfqRkJFWWD447WgouP184FlS1EkJ6CTJajYT5G0A',
+	access_token_secret: 'uzLRrD1ZAi2UgNk12kK7F6HQq4AXZNW8rG8mbsR2z6WsI'
+	});
 
 //  define how requests to the http server will be handled \\
 
 function requestHandler(request, response){
-	fs.readFile(__dirname + '/' + 'index.html', function(err, data) {
+	// fs.readFile(__dirname + '/' + 'index.html', function(err, data) {
 		if(!err) {
 			response.writeHead(200, {'Content-Type': 'text/html'});
+			response.write('<h1>server serving</h1>')
 			response.end(data);
 		} else {
 			response.writeHead(404, {'Content-Type': 'text/html'});
 			response.end('<h1>404 page not found</h1>');
 		}
-	})
 }
 
-twitObj = {(
-	'user_key': placeholder
-	'user_secret)key': placeholder
-	'user_token': placeholder
-	'user_secret_token': placeholder
-	)}
 
+// creating twitter stream \\
+// --------------------------
 
-// twitter API request or  stream will go here \\
-// -----------------------------------
-
-
+client.stream('statuses/filter', {track: '@founderscoders'}, function(stream) {
+	stream.on('data', function(tweet) {
+		console.log(tweet.text);
+	});
+	stream.on('error', function(error){
+		console.log(error);
+	});
+});
 
 
 
